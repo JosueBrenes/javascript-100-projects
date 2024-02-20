546c9adbca017d78797ee059f1fe8447d41b4f68
Josué Brenes <123853174+JosueBrenes@users.noreply.github.com>
2024-02-20 14:03:49 -0600

    first commit

---
+++ web/src/consts.js
@@ -0,0 +1,27 @@
+export const PROJECTS = [
+  {
+    slug: "01-tinder-swipe",
+    title: "Tinder Swipe",
+    description:
+      "Aprende a crear el efecto de swipe de Tinder con HTML, CSS y JavaScript.",
+    learnings: [
+      "DOM",
+      "Animaciones con gestos",
+      "Eventos de mouse y touch",
+      "Transiciones CSS",
+    ],
+    youtube: 'https://www.youtube.com/watch?v=u01WD_YNENY'
+  },
+  {
+    slug: "02-arkanoid-game",
+    title: "Arkanoid Game",
+    description:
+      "Juego mítico y clásico de Arkanoid para controlar con teclado",
+    learnings: [
+      "Dibujar en Canvas",
+      "Eventos de teclado",
+      "RequestAnimationFrame",
+      "Sprites"
+    ]
+  },
+]
